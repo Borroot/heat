aa51ed4d1b7e45692f323121dfa09f89a95c634f
Borroot <borroot@live.nl>
2020-05-23 14:19:50 +0200

    Fix tabs.

--- src/relax_fast.c
+++ src/relax_fast.c
@@ -3,19 +3,19 @@
 #include <stdbool.h>
 #include <math.h>
 
-//#define N 10000000   // length of the vectors
+//#define N 10000000  // length of the vectors
 #define N 100
-#define EPS 0.00001      // convergence criterium
-#define HEAT 10.0   // heat value on the boundary
+#define EPS 0.00001   // convergence criterium
+#define HEAT 10.0     // heat value on the boundary
 
 //
 // allocate a vector of length "n"
 // malloc nullptr is not being checked
 double *allocVector(int n)
 {
-   double *v;
-   v = (double *)malloc( n*sizeof(double));
-   return v;
+	double *v;
+	v = (double *)malloc( n*sizeof(double));
+	return v;
 }
 
 //
@@ -23,12 +23,12 @@ double *allocVector(int n)
 //
 void init( double *out, int n)
 {
-   int i;
+	int i;
 
-   for( i=1; i<n; i++) {
-      out[i] = 0;
-   }
-   out[0] = HEAT;
+	for( i=1; i<n; i++) {
+		out[i] = 0;
+	}
+	out[0] = HEAT;
 
 }
 
@@ -37,13 +37,13 @@ void init( double *out, int n)
 //
 void print( double *out, int n)
 {
-   int i;
+	int i;
 
-   printf( "<");
-   for( i=0; i<n; i++) {
-      printf( " %f", out[i]);
-   }
-   printf( ">\n");
+	printf( "<");
+	for( i=0; i<n; i++) {
+		printf( " %f", out[i]);
+	}
+	printf( ">\n");
 }
 
 //
@@ -53,9 +53,9 @@ void print( double *out, int n)
 //
 void relax( double *in, double *out, int n)
 {
-   for(int i=1; i<n-1; i++) {
-      out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
-   }
+	for(int i=1; i<n-1; i++) {
+		out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
+	}
 }
 
 //
@@ -64,63 +64,60 @@ void relax( double *in, double *out, int n)
 //
 bool isStable( double *old, double *new, int n, double eps)
 {
-   for(int i=1; i<n-1; i++) {
-      if(!(fabs(old[i] - new[i]) <= eps)) return false;
-   }
-   return true;
+	for(int i=1; i<n-1; i++) {
+		if(!(fabs(old[i] - new[i]) <= eps)) return false;
+	}
+	return true;
 }
 
 bool relaxAndStable(double *in, double *out, int n, double eps) {
-   bool notstable = true;
-   for(int i = 1; i < n-1; i++) {
-      out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
-      if(notstable) {
-         notstable = notstable && (fabs(out[i] - in[i]) <= eps);
-      }
-   }
-   return notstable;
+	bool notstable = true;
+	for(int i = 1; i < n-1; i++) {
+		out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
+		if(notstable) {
+			notstable = notstable && (fabs(out[i] - in[i]) <= eps);
+		}
+	}
+	return notstable;
 }
 
 int main()
 {
-   double *a,*b, *tmp;
-   int n;
-   int iterations = 0;
+	double *a,*b, *tmp;
+	int n;
+	int iterations = 0;
 
-   a = allocVector(N);
-   b = allocVector(N);
+	a = allocVector(N);
+	b = allocVector(N);
 
-   init(a, N);
-   init(b, N);
+	init(a, N);
+	init(b, N);
 
-   n = N;
+	n = N;
 
-   printf("size   : %d M (%d MB)\n", n/1000000, (int)(n*sizeof(double) / (1024*1024)));
-   printf("heat   : %f\n", HEAT);
-   printf("epsilon: %f\n", EPS);
+	printf("size   : %d M (%d MB)\n", n/1000000, (int)(n*sizeof(double) / (1024*1024)));
+	printf("heat   : %f\n", HEAT);
+	printf("epsilon: %f\n", EPS);
 
+	#if RELAX_AND_STABLE == 1
+		do {
+			tmp = a;
+			a = b;
+			b = tmp;
+			iterations++;
+		} while(!relaxAndStable(a, b, n, EPS));
+	#else
+		do {
+			tmp = a;
+			a = b;
+			b = tmp;
+			relax(a, b, n);
+			iterations ++;
+		} while(!isStable(a, b, n, EPS));
+	#endif
 
+	print(b, n);
+	printf("Number of iterations: %d\n", iterations);
 
-#if RELAX_AND_STABLE == 1
-   do {
-      tmp = a;
-      a = b;
-      b = tmp;
-      iterations++;
-   } while(!relaxAndStable(a, b, n, EPS));
-#else
-   do {
-      tmp = a;
-      a = b;
-      b = tmp;
-      relax(a, b, n);
-      iterations ++;
-   } while(!isStable(a, b, n, EPS));
-#endif
-
-   print(b, n);
-
-   printf("Number of iterations: %d\n", iterations);
-
-   return 0;
+	return 0;
 }
--- src/relax_orig.c
+++ src/relax_orig.c
@@ -9,9 +9,9 @@
 //
 double *allocVector( int n)
 {
-   double *v;
-   v = (double *)malloc( n*sizeof(double));
-   return v;
+	double *v;
+	v = (double *)malloc( n*sizeof(double));
+	return v;
 }
 
 //
@@ -19,12 +19,12 @@ double *allocVector( int n)
 //
 void init( double *out, int n, int h)
 {
-   int i;
+	int i;
 
-   for( i=1; i<n; i++) {
-      out[i] = 0;
-   }
-   out[0] = h;
+	for( i=1; i<n; i++) {
+		out[i] = 0;
+	}
+	out[0] = h;
 
 }
 
@@ -33,13 +33,13 @@ void init( double *out, int n, int h)
 //
 void print( double *out, int n)
 {
-   int i;
+	int i;
 
-   printf( "<");
-   for( i=0; i<n; i++) {
-      printf( " %f", out[i]);
-   }
-   printf( ">\n");
+	printf( "<");
+	for( i=0; i<n; i++) {
+		printf( " %f", out[i]);
+	}
+	printf( ">\n");
 }
 
 //
@@ -49,10 +49,10 @@ void print( double *out, int n)
 //
 void relax( double *in, double *out, int n)
 {
-   int i;
-   for( i=1; i<n-1; i++) {
-      out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
-   }
+	int i;
+	for( i=1; i<n-1; i++) {
+		out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
+	}
 }
 
 //
@@ -61,52 +61,52 @@ void relax( double *in, double *out, int n)
 //
 bool isStable( double *old, double *new, int n, double eps)
 {
-   int i;
-   bool res=true;
+	int i;
+	bool res=true;
 
-   for( i=1; i<n-1; i++) {
-      res = res && ( fabs( old[i] - new[i]) <= eps);
-   }
-   return res;
+	for( i=1; i<n-1; i++) {
+		res = res && ( fabs( old[i] - new[i]) <= eps);
+	}
+	return res;
 }
 
 // Commandline arguments:
 // <N> <EPS> <HEAT>
 int main(int argc, char *argv[])
 {
-   double N = 10000000.0;   // length of the vectors
-   double EPS = 0.1;      // convergence criterium
-   double HEAT = 100.0;   // heat value on the boundary
+	double N = 10000000.0;  // length of the vectors
+	double EPS = 0.1;       // convergence criterium
+	double HEAT = 100.0;    // heat value on the boundary
 
-   if (argc == 4) {
-      sscanf(argv[1],"%lf",&N);
-      sscanf(argv[2],"%lf",&EPS);
-      sscanf(argv[3],"%lf",&HEAT);
-   }
+	if (argc == 4) {
+		sscanf(argv[1], "%lf", &N);
+		sscanf(argv[2], "%lf", &EPS);
+		sscanf(argv[3], "%lf", &HEAT);
+	}
 
-   double *a,*b, *tmp;
-   int iterations = 0;
+	double *a,*b, *tmp;
+	int iterations = 0;
 
-   a = allocVector( N);
-   b = allocVector( N);
+	a = allocVector( N);
+	b = allocVector( N);
 
-   init(a, N, HEAT);
-   init(b, N, HEAT);
+	init(a, N, HEAT);
+	init(b, N, HEAT);
 
-   printf("size   : %d M (%d MB)\n", N/1000000, (int)(N*sizeof(double) / (1024*1024)));
-   printf("epsilon: %f\n", EPS);
-   printf("heat   : %f\n", HEAT);
+	printf("size   : %d M (%d MB)\n", N/1000000, (int)(N*sizeof(double) / (1024*1024)));
+	printf("epsilon: %f\n", EPS);
+	printf("heat   : %f\n", HEAT);
 
-   do {
-      tmp = a;
-      a = b;
-      b = tmp;
-      relax(a, b, N);
-      // print(b, n);
-      iterations ++;
-   } while(!isStable(a, b, N, EPS));
+	do {
+		tmp = a;
+		a = b;
+		b = tmp;
+		relax(a, b, N);
+		// print(b, n);
+		iterations ++;
+	} while(!isStable(a, b, N, EPS));
 
-   printf("Number of iterations: %d\n", iterations);
+	printf("Number of iterations: %d\n", iterations);
 
-   return 0;
+	return 0;
 }
