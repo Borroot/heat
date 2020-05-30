74909ce037394829a9ce1b161026ffe9fc4c69c6
Wouter Damen <damen.wouter@gmail.com>
2020-05-30 14:56:07 +0200

    MPI gather+reduce versions

---
+++ src/mpi/gather/relax_split2.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+double *allocVector(int n)
+{
+	double *vector = (double *)malloc(n * sizeof(double));
+	if(vector) {
+		return vector;
+	} else {
+		exit(1);
+	}
+}
+
+void init(double *out, int n, int heat)
+{
+	for (int i = 1; i < n; i++) {
+		out[i] = 0;
+	}
+	out[0] = heat;
+}
+
+void relax(double* in, double* out, int n) {
+	for(int i = 1; i < n-1; i++) {
+		out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
+	}
+}
+
+int isStable(double* in, double* out, int n, double eps) {
+	for(int i = 1; i < n-1; i++) {
+		if(fabs(out[i] - in[i]) > eps) return 0;
+	}
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	double N = 100000.0;    // length of the vectors
+	double EPS = 0.1;       // convergence criterium
+	double HEAT = 100.0;    // heat value on the boundary
+
+	if (argc == 4) {
+		sscanf(argv[1], "%lf", &N);
+		sscanf(argv[2], "%lf", &EPS);
+		sscanf(argv[3], "%lf", &HEAT);
+	}
+
+	MPI_Init(&argc, &argv);
+	double start = MPI_Wtime();
+
+	int num_ranks, my_rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    int L = N / num_ranks;
+
+	double *a = allocVector(N);
+	double *b = allocVector(N);
+    double *sub = allocVector(L);
+
+	init(a, N, HEAT);
+	init(b, N, HEAT);
+    init(sub, L, 0);
+    if(my_rank == 0) sub[0] = HEAT;
+
+	double *tmp;
+    int stable;
+	do {
+		tmp = a;
+		a = b;
+		b = tmp;
+        relax(a+(L*my_rank), sub, L);
+        MPI_Allgather(sub, L, MPI_DOUBLE, b, L, MPI_DOUBLE, MPI_COMM_WORLD);
+        stable = 1;
+		int part_stable = isStable(a+(L*my_rank), sub, L, EPS);
+        MPI_Allreduce(&part_stable, &stable, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+	} while(!stable);
+
+	double end = MPI_Wtime();
+	MPI_Finalize();
+
+	if(my_rank == 0) {
+		printf("%f\n", end - start);
+	}
+
+	return 0;
+}
---
+++ src/mpi/gather/relax_splitzero.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+double *allocVector(int n)
+{
+	double *vector = (double *)malloc(n * sizeof(double));
+	if(vector) {
+		return vector;
+	} else {
+		exit(1);
+	}
+}
+
+void init(double *out, int n, int heat)
+{
+	for (int i = 1; i < n; i++) {
+		out[i] = 0;
+	}
+	out[0] = heat;
+}
+
+void relax(double* in, double* out, int n) {
+	for(int i = 1; i < n-1; i++) {
+		out[i] = 0.25*in[i-1] + 0.5*in[i] + 0.25*in[i+1];
+	}
+}
+
+int isStable(double* in, double* out, int n, double eps) {
+	for(int i = 1; i < n-1; i++) {
+		if(fabs(out[i] - in[i]) > eps) return 0;
+	}
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	double N = 100000.0;    // length of the vectors
+	double EPS = 0.1;       // convergence criterium
+	double HEAT = 100.0;    // heat value on the boundary
+
+	if (argc == 4) {
+		sscanf(argv[1], "%lf", &N);
+		sscanf(argv[2], "%lf", &EPS);
+		sscanf(argv[3], "%lf", &HEAT);
+	}
+
+	MPI_Init(&argc, &argv);
+	double start = MPI_Wtime();
+
+	int num_ranks, my_rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    double L = N / num_ranks;
+
+	double *a = allocVector(N);
+	double *b = allocVector(N);
+    double *sub = allocVector((int)ceil(L));
+
+	init(a, N, HEAT);
+	init(b, N, HEAT);
+    init(sub, L, 0);
+    if(my_rank == 0) sub[0] = HEAT;
+
+	double *tmp;
+    int stable;
+	int zero = 3;
+	do {
+		tmp = a;
+		a = b;
+		b = tmp;
+		if (zero < N) zero += 1;
+		L = zero / num_ranks;
+		int s = ceil(L*my_rank);
+		int l = ceil(L);
+        relax(a+s, sub, l);
+		int part_stable = isStable(a+s, sub, l, EPS);
+        MPI_Allgather(sub, l, MPI_DOUBLE, b, l, MPI_DOUBLE, MPI_COMM_WORLD);
+        stable = 1;
+        MPI_Allreduce(&part_stable, &stable, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+	} while(!stable);
+
+	double end = MPI_Wtime();
+	MPI_Finalize();
+
+	if(my_rank == 0) {
+		printf("%f\n", end - start);
+	}
+
+	return 0;
+}
